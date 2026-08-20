4dbc508e4eb067215883caf91de2ce18b77b88c0
plutonaruto <charistan297@gmail.com>
2026-08-21 01:27:17 +0800

    Add greeting

---
+++ src/main/java/Phin.java
@@ -0,0 +1,14 @@
+/**
+ * Starts the Phin chatbot application.
+ */
+public class Phin {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Phin");
+        System.out.println("Hello! I'm Phin, your remarkably chipper chatbot!");
+        System.out.println("What can I do for you today -- besides juggle imaginary data?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye! Stay silly, and may your bugs be tiny!");
+        System.out.println("____________________________________________________________");
+    }
+}
