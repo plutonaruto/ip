a056ff566554cb98e13a60556e42818b3758a25f
plutonaruto <charistan297@gmail.com>
2026-08-21 01:36:11 +0800

    Add support for echo

--- src/main/java/Phin.java
+++ src/main/java/Phin.java
@@ -1,14 +1,36 @@
+import java.util.Scanner;
+
 /**
  * Starts the Phin chatbot application.
  */
 public class Phin {
+    private static final String DIVIDER = "____________________________________________________________";
+
+    /**
+     * Runs Phin's command loop, echoing input until the user enters {@code bye}.
+     *
+     * @param args command-line arguments; not used by this application
+     */
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(DIVIDER);
         System.out.println("Phin");
-        System.out.println("Hello! I'm Phin, your remarkably chipper chatbot!");
-        System.out.println("What can I do for you today -- besides juggle imaginary data?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye! Stay silly, and may your bugs be tiny!");
-        System.out.println("____________________________________________________________");
+        System.out.println("I'm Phin. Apparently I have to deal with this.");
+        System.out.println("What do you want?");
+        System.out.println(DIVIDER);
+
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println("    Finally. Bye.");
+                System.out.println(DIVIDER);
+                break;
+            }
+
+            System.out.println("    " + command + ". Yeah, I heard you the first time.");
+            System.out.println(DIVIDER);
+        }
     }
 }
